b6fbff9fe0a473392b656ff6479d9b87619a1f96
dominikus1993 <dominikus1910@gmail.com>
2020-09-12 20:06:20 +0200

    add secrets and configmaps

--- src/f8s/Enviroment.fs
+++ src/f8s/Enviroment.fs
@@ -8,13 +8,19 @@ module Environment =
         | ConfigMap of name: string
         | Secret of name: string
     type EnvironmentState = { Variables: EnvironmentVariable list }
-    
+
+    let private mapConfig (env: EnvironmentVariable) =
+        match env with
+        | NameValue(name, value) -> Choice1Of2(V1EnvVar(name, value))
+        | ConfigMap(name) -> Choice2Of2(V1EnvFromSource(configMapRef = V1ConfigMapEnvSource(name)))
+        | Secret(name) -> Choice2Of2(V1EnvFromSource(secretRef = V1SecretEnvSource(name)))
+        
     type EnvironmentBuilder internal () =
         member this.Yield(_) =
             { Variables = []; }
         
         member this.Run(state: EnvironmentState) = 
-            state.Variables |> List.map(fun e -> match e with | NameValue(name, value) -> Choice1Of2(V1EnvVar(name, value)))
+            state.Variables |> List.map(fun e -> mapConfig(e))
             
 
         [<CustomOperation("add_var")>]
