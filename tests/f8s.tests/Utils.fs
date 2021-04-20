f114c481969d7a3a31d54d60fb45abc9ea7adb75
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2021-04-20 09:23:31 +0200

    add test for utils

---
+++ tests/f8s.tests/Utils.fs
@@ -0,0 +1,14 @@
+module UtilsTests
+
+open System
+open Xunit
+open k8s
+open k8s.Models
+open FsUnit.Xunit
+open FSharpNetes.Utils
+
+[<Fact>]
+let ``Test to list`` () =
+    let list = [1;2;3;4]
+    let subject = list |> toList
+    subject |> should haveCount 4
