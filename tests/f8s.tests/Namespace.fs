c15aeafe6d3a9f8072113cf8f4cd68140304d3c8
dominikus1993 <dominikus1910@gmail.com>
2021-04-20 19:24:44 +0200

    add test

---
+++ tests/f8s.tests/Namespace.fs
@@ -0,0 +1,22 @@
+module NamespaceTests
+
+open System
+open Xunit
+open k8s
+open k8s.Models
+open FsUnit.Xunit
+open FSharpNetes
+
+[<Fact>]
+let ``Test one name value env`` () =
+    let meta = metadata {
+        name "test"
+        label (Label("test", "test"))
+    }
+    let testNamespace = nmspc {
+        metadata meta
+    }
+    
+    testNamespace.Metadata.Name |> should equal "test"
+    let label = testNamespace.Metadata.Labels.["test"]
+    label |> should equal "test"
