0c5d7e69383a37ed87d52c64cbe7b69d2fc32d1e
dominikus1993 <dominikus1910@gmail.com>
2021-09-19 16:10:49 +0000

    small changes

--- examples/nginx-pod/Program.fs
+++ examples/nginx-pod/Program.fs
@@ -10,19 +10,7 @@ let main argv =
         label (Label("test", "test"))
     }
     
-    let ngix = container {
-        name "nginx"
-        image (Image("nginx", Latest))
-        image_pull_policy Always
-        env [NameValue("Test", "22312"); NameValue("Test2", "22312")]
-    }
-    
-    
-    let nginxPod = pod {
-        metadata meta
-        container ngix
-    }
 
-    let yaml = nginxPod |> Serialization.toYaml
+    let yaml = meta |> Serialization.toYaml
     printfn $"{yaml}"
     0 // return an integer exit code
