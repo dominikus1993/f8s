c5f2ab3468c3a26e759dc865597c4fac78905c47
dominikus1993 <dominikus1910@gmail.com>
2021-10-03 17:11:49 +0000

    add Env

--- src/f8s/Pod.fs
+++ src/f8s/Pod.fs
@@ -1,1 +1,20 @@
 namespace FSharpNetes
+open k8s.Models
+open k8s
+
+[<AutoOpen>]
+module Pod =
+    type PodState = { MetaData: V1ObjectMeta option }
+    
+    type PodBuilder internal () =
+        member this.Yield(_) =
+            { MetaData = None; }
+        
+        member this.Run(state: NamespaceState) = 
+            let meta = defaultArg state.MetaData (V1ObjectMeta())
+            V1Pod(metadata = meta)
+
+        [<CustomOperation("metadata")>]
+        member this.Name (state: NamespaceState, meta: V1ObjectMeta) =
+            { state with MetaData = Some(meta) }
+            
