a4643cae75394788f121bd4a7620bcb0836f7ab2
Dominik Kotecki <dominikus1910@gmail.com>
2021-10-23 18:26:23 +0200

    selector

--- src/f8s/Deployment.fs
+++ src/f8s/Deployment.fs
@@ -4,35 +4,46 @@ open k8s
 
 [<AutoOpen>]
 module Deployment =
+
+    type private SelectorState = { MatchLabels: Map<string, string> }
+
+    type Selector = 
+        | MatchLabels of key: string * value: string
+
+    type Deploymenttate = { MetaData: V1ObjectMeta option; Pod: V1PodSpec option; Selectors: Selector list option  }
     
-    type Deploymenttate = { MetaData: V1ObjectMeta option; Containers: V1Container list option }
-    
+    let private mapSelectors (selectors: Selector list) =
+        let mapSelector selector 
+            match s with
+             MatchLabels(k, v) ->
+                V1LabelSelector(matchLabels = )
+
     type DeploymentBuilder internal () =
         member this.Yield(_) =
-            { MetaData = None; Containers = None }
+            { MetaData = None; Pod = None; Selector = None }
         
         member this.Run(state: Deploymenttate) = 
             let meta = defaultArg state.MetaData (V1ObjectMeta())
-            let containers = defaultArg state.Containers [] |> Utils.toList
-            
-            let spec = V1DeploymentSpec(selector = V1LabelSelector(),template = V1PodTemplateSpec(spec = V1PodSpec(containers = containers)))
+            let pod = defaultArg state.Pod null
+            let selector = state.Selectors |> Option.map(fun s -> )
+            let spec = V1DeploymentSpec(selector = V1LabelSelector(), template = V1PodTemplateSpec(spec = pod))
             V1Deployment(metadata = meta, spec = spec)
 
         [<CustomOperation("metadata")>]
         member this.Name (state: Deploymenttate, meta: V1ObjectMeta) =
             { state with MetaData = Some(meta) }
 
-        [<CustomOperation("container")>]
-        member this.Container (state: Deploymenttate, containers: V1Container) =
-            this.Containers(state, [containers])
+        [<CustomOperation("pod")>]
+        member this.Container (state: Deploymenttate, pod: V1PodSpec) =
+            { state with Pod = Some(pod) }
 
-        [<CustomOperation("containers")>]
-        member this.Containers (state: Deploymenttate, containers: V1Container list) =
-            match state.Containers with
-            | Some(cont) ->
-                { state with Containers = Some(cont @ containers)}
-            | None ->
-                { state with Containers = Some(containers)}
+        [<CustomOperation("selector")>]
+        member this.AddSelector (state: Deploymenttate, selector: Selector) =
+            match state.Selectors with
+            | Some(selectors) ->
+                { state with Selectors = Some(selectors @ [selector]) }
+            | None -> 
+                { state with Selectors = Some([selector])}
 
     
     let deployment = DeploymentBuilder()
