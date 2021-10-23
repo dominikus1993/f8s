2870316e218ed71db50d3659881a4d86d55c900a
dominikus1993 <dominikus1910@gmail.com>
2021-10-23 09:47:28 +0200

    small change

--- src/f8s/Deployment.fs
+++ src/f8s/Deployment.fs
@@ -14,7 +14,9 @@ module Deployment =
         member this.Run(state: Deploymenttate) = 
             let meta = defaultArg state.MetaData (V1ObjectMeta())
             let containers = defaultArg state.Containers [] |> Utils.toList
-            V1Pod(metadata = meta, spec = V1PodSpec(containers = containers))
+
+            let spec = V1DeploymentSpec(selector = V1LabelSelector(),template = V1PodTemplateSpec(spec = V1PodSpec(containers = containers)))
+            V1Deployment(metadata = meta, spec = spec)
 
         [<CustomOperation("metadata")>]
         member this.Name (state: Deploymenttate, meta: V1ObjectMeta) =
