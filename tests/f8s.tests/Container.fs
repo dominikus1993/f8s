07c1dc7424c348c561adfaeadc5861fcf3c52c42
Dominik Kotecki <dominikus1910@gmail.com>
2021-10-24 08:45:07 +0200

    add simple test to container

--- tests/f8s.tests/Container.fs
+++ tests/f8s.tests/Container.fs
@@ -1,1 +1,24 @@
 module ContainerTests
+open FSharpNetes
+open Xunit
+open FsUnit.Xunit
+open k8s.Models
+
+[<Fact>]
+let ``Test container`` () =
+    let nginxCont = container {
+        name "nginx"
+        image (Image("nginx", Latest))
+        image_pull_policy (IfNotPresent)
+        command ["nginx"; "-g"; "daemon off;"]
+        env [NameValue("PORT", "8080")]
+        ports [TCP(8080)]
+    }   
+
+    nginxCont.Name |> should equal "nginx"
+    nginxCont.Image |> should equal "nginx:latest"
+    nginxCont.ImagePullPolicy |> should equal "IfNotPresent"
+    nginxCont.Env |> should haveCount 1
+    let env = nginxCont.Env.[0]
+    env.Name |> should equal "PORT"
+    env.Value |> should equal "8080"
