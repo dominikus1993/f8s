902d85de305943a2ea58383477ac28db0cef2ba1
Dominik Kotecki <dominikus1910@gmail.com>
2021-10-24 12:34:33 +0200

    add pod tests

--- tests/f8s.tests/Pod.fs
+++ tests/f8s.tests/Pod.fs
@@ -5,7 +5,7 @@ open FsUnit.Xunit
 
 
 [<Fact>]
-let ``Test image with latest tag`` () =
+let ``Test pod`` () =
     let meta = metadata {
         name "test"
         nmspc "test"
@@ -26,3 +26,18 @@ let ``Test image with latest tag`` () =
         container nginxCont
     }
     nginxPod.Metadata.Name |> should equal meta.Name
+    nginxPod.Metadata.NamespaceProperty |> should equal meta.NamespaceProperty
+    nginxPod.Spec.Containers |> should haveCount 1
+    let cont = nginxPod.Spec.Containers.[0]
+    cont.Name |> should equal "nginx"
+    cont.Image |> should equal "nginx:latest"
+    cont.ImagePullPolicy |> should equal "IfNotPresent"
+    cont.Env |> should haveCount 1
+    let env = cont.Env.[0]
+    env.Name |> should equal "PORT"
+    env.Value |> should equal "8080"
+    cont.Ports |> should haveCount 1
+    let port = cont.Ports.[0]
+    port.Protocol |> should equal "TCP"
+    port.ContainerPort |> should equal 8080
+    cont.Command |> Seq.toList |> should matchList (["nginx"; "-g"; "daemon off;"])
