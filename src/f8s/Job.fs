e03fe795cae9a6ceb5f08c8e220bbd76950daf7c
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-08 19:49:20 +0100

    test

--- src/f8s/Job.fs
+++ src/f8s/Job.fs
@@ -1,1 +1,21 @@
-namespace Job
+namespace FSharpNetes
+
+open k8s.Models
+
+[<AutoOpen>]
+module Job =
+
+    type private SelectorState = { MatchLabels: Map<string, string> }
+    
+    type Selector = 
+        | MatchLabels of key: string * value: string
+
+    type JobState = { MetaData: V1ObjectMeta option; JobSpec: V1JobSpec option; }
+
+
+    type PodBuilder internal () =
+        member this.Yield(_) =
+            { MetaData = None; Containers = None }
+        
+        member this.Run(state: PodState) = 
+            V1Job(apiVersion = "v1", kind = "Pod")
