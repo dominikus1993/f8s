05e68730376683dbe7478f83c0bd338e5268f415
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-09 18:07:13 +0100

    add simple test for cronjob

--- src/f8s/Job.fs
+++ src/f8s/Job.fs
@@ -13,9 +13,17 @@ module Job =
     type JobState = { MetaData: V1ObjectMeta option; JobSpec: V1JobSpec option; }
 
 
-    type PodBuilder internal () =
+    type JobBuilder internal () =
         member this.Yield(_) =
-            { MetaData = None; Containers = None }
+            { MetaData = None; JobSpec = None }
         
-        member this.Run(state: PodState) = 
-            V1Job(apiVersion = "v1", kind = "Pod")
+        member this.Run(state: JobState) = 
+            let metadata = defaultArg state.MetaData null
+            V1Job(apiVersion = "batch/v1", kind = "Job", metadata = metadata)
+
+        [<CustomOperation("metadata")>]
+        member _.Name (state: JobState, meta: V1ObjectMeta) = 
+            { state with MetaData = Some(meta) }
+    
+
+    let job = JobBuilder()
--- tests/f8s.tests/Job.fs
+++ tests/f8s.tests/Job.fs
@@ -1,1 +1,25 @@
 module JobTests
+
+open System
+open Xunit
+open k8s
+open k8s.Models
+open FsUnit.Xunit
+open FSharpNetes
+
+[<Fact>]
+let ``Test image with semVer tag`` () =
+    let meta = metadata {
+        name "test"
+        nmspc "test"
+        labels [Label("app", "test"); Label("server", "nginx")]
+    }
+    let nginxJob = job { 
+        metadata meta
+    }
+
+    nginxJob.Kind |> should equal "Job"
+    nginxJob.ApiVersion |> should equal "batch/v1"
+    nginxJob.Metadata.Name |> should equal meta.Name
+    nginxJob.Metadata.NamespaceProperty |> should equal meta.NamespaceProperty
+
