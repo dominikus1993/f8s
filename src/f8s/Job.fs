0cbd1cef59af98ec123426e4b118b0ce20ea07f2
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-10 17:41:20 +0100

    add cron job spec

--- src/f8s/Job.fs
+++ src/f8s/Job.fs
@@ -3,27 +3,52 @@ namespace FSharpNetes
 open k8s.Models
 
 [<AutoOpen>]
-module Job =
+module CronJobSpec =
+    open System
+
+    type CronJobSpecState = { Schedule: string option } 
+    type CronJobSpecBuilder internal () =
+        member this.Yield(_) =
+            { Schedule = None }
+        
+        member this.Run(state: CronJobSpecState) = 
+            let schedule = defaultArg state.Schedule null
+            let template = V1JobTemplateSpec()
+            V1CronJobSpec(template, schedule = schedule)
+
+        [<CustomOperation("schedule")>]
+        member _.Name (state: CronJobSpecState, schedule: string) = 
+            { state with Schedule = schedule |> Option.ofObj |> Option.filter(fun x -> x |> String.IsNullOrEmpty |> not) }
+
+    let cronJobSpec = CronJobSpecBuilder()
+
+[<AutoOpen>]
+module CronJob =
 
     type private SelectorState = { MatchLabels: Map<string, string> }
     
     type Selector = 
         | MatchLabels of key: string * value: string
 
-    type JobState = { MetaData: V1ObjectMeta option; JobSpec: V1JobSpec option; }
+    type CronJobState = { MetaData: V1ObjectMeta option; JobSpec: V1CronJobSpec option; }
 
 
-    type JobBuilder internal () =
+    type CronJobBuilder internal () =
         member this.Yield(_) =
             { MetaData = None; JobSpec = None }
         
-        member this.Run(state: JobState) = 
+        member this.Run(state: CronJobState) = 
             let metadata = defaultArg state.MetaData null
-            V1Job(apiVersion = "batch/v1", kind = "Job", metadata = metadata)
+            let spec = defaultArg state.JobSpec null
+            V1CronJob(apiVersion = "batch/v1", kind = "CronJob", metadata = metadata, spec = spec)
 
         [<CustomOperation("metadata")>]
-        member _.Name (state: JobState, meta: V1ObjectMeta) = 
-            { state with MetaData = Some(meta) }
+        member _.Name (state: CronJobState, meta: V1ObjectMeta) = 
+            { state with MetaData = meta |> Option.ofObj }
+
+        [<CustomOperation("spec")>]
+        member _.Spec (state: CronJobState, spec: V1CronJobSpec) = 
+            { state with JobSpec = (Option.ofObj spec) }
     
 
-    let job = JobBuilder()
+    let cronjob = CronJobBuilder()
--- tests/f8s.tests/Job.fs
+++ tests/f8s.tests/Job.fs
@@ -14,11 +14,15 @@ let ``Test image with semVer tag`` () =
         nmspc "test"
         labels [Label("app", "test"); Label("server", "nginx")]
     }
-    let nginxJob = job { 
+    let nginxSpec = cronJobSpec {
+        schedule "* * * * *"
+    }
+    let nginxJob = cronjob { 
         metadata meta
+        spec nginxSpec
     }
 
-    nginxJob.Kind |> should equal "Job"
+    nginxJob.Kind |> should equal "CronJob"
     nginxJob.ApiVersion |> should equal "batch/v1"
     nginxJob.Metadata.Name |> should equal meta.Name
     nginxJob.Metadata.NamespaceProperty |> should equal meta.NamespaceProperty
