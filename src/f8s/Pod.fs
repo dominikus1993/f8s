a77e7b82f72525dfd4cf5dc8c40a26bf7b0d4130
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-27 11:23:16 +0100

    add image pull secret

--- src/f8s/Pod.fs
+++ src/f8s/Pod.fs
@@ -4,16 +4,30 @@ open k8s
 
 [<AutoOpen>]
 module PodSpec = 
-    type PodSpecState = { Containers: V1Container list option; SecurityContext: V1PodSecurityContext option}
+    type PodSpecState = { Containers: V1Container list option; SecurityContext: V1PodSecurityContext option; ImagePullSecrets: V1LocalObjectReference list option}
 
     type PodSpecBuilder internal () =
         member this.Yield(_) =
-            { Containers = None; SecurityContext = None }
+            { Containers = None; SecurityContext = None; ImagePullSecrets = None }
         
         member this.Run(state: PodSpecState) = 
             let containers = defaultArg state.Containers [] |> Utils.toList
             let security = defaultArg state.SecurityContext null
-            V1PodSpec(containers = containers, securityContext = security)
+            let imagePullSecrets = defaultArg (state.ImagePullSecrets |> Option.map(fun x -> x |> Utils.toList)) null
+            V1PodSpec(containers = containers, securityContext = security, imagePullSecrets = imagePullSecrets)
+
+        [<CustomOperation("imagePullSecret")>]
+        member this.ImagePullSecret (state: PodSpecState, secret: string) =
+            this.ImagePullSecrets(state, [secret])
+
+        [<CustomOperation("imagePullSecrets")>]
+        member this.ImagePullSecrets (state: PodSpecState, secrets: string list) =
+            let s = secrets |> List.map(fun secret -> V1LocalObjectReference(secret))
+            match state.ImagePullSecrets with
+            | Some(cont) ->
+                { state with ImagePullSecrets = Some(cont @ s)}
+            | None ->
+                { state with ImagePullSecrets = Some(s)}
 
         [<CustomOperation("container")>]
         member this.Container (state: PodSpecState, containers: V1Container) =
@@ -31,11 +45,11 @@ module PodSpec =
     let podSpec = PodSpecBuilder()
 [<AutoOpen>]
 module Pod =
-    type PodState = { MetaData: V1ObjectMeta option; Spec: V1PodSpec option}
+    type PodState = { MetaData: V1ObjectMeta option; Spec: V1PodSpec option; }
     
     type PodBuilder internal () =
         member this.Yield(_) =
-            { MetaData = None; Spec = None }
+            { MetaData = None; Spec = None;}
         
         member this.Run(state: PodState) = 
             let meta = defaultArg state.MetaData (V1ObjectMeta())
@@ -50,6 +64,23 @@ module Pod =
         member this.Container (state: PodState, containers: V1Container) =
             this.Containers(state, [containers])
 
+        [<CustomOperation("imagePullSecrets")>]
+        member this.ImagePullSecret (state: PodState, secret: string) =
+            this.ImagePullSecrets(state, [secret])
+
+        [<CustomOperation("containers")>]
+        member this.ImagePullSecrets (state: PodState, secrets: string list) =
+            let s = secrets |> List.map(fun secret -> V1LocalObjectReference(secret)) |> Utils.toList
+            match state.Spec with
+            | Some(spec) ->
+                spec.ImagePullSecrets <- s 
+                { state with Spec = Some(spec)}
+            | None ->
+                let spec = 
+                    podSpec {
+                        imagePullSecrets secrets
+                    }
+                { state with Spec = Some(spec)}
         [<CustomOperation("spec")>]
         member this.Spec (state: PodState, spec: V1PodSpec) =
             { state with Spec = (spec |> Option.ofObj)}
--- tests/f8s.tests/Pod.fs
+++ tests/f8s.tests/Pod.fs
@@ -91,3 +91,96 @@ let ``Test pod`` () =
     port.Protocol |> should equal "TCP"
     port.ContainerPort |> should equal 8080
     cont.Command |> Seq.toList |> should matchList (["nginx"; "-g"; "daemon off;"])
+
+
+[<Fact>]
+let ``Test pod with image pull secret`` () =
+    let meta = metadata {
+        name "test"
+        nmspc "test"
+        labels [Label("app", "test"); Label("server", "nginx")]
+    }
+
+    let nginxCont = container {
+        name "nginx"
+        image (Image("nginx", Latest))
+        image_pull_policy (IfNotPresent)
+        command ["nginx"; "-g"; "daemon off;"]
+        env [NameValue("PORT", "8080")]
+        ports [TCP(8080)]
+    }   
+
+    let nginxPodSpec = podSpec {
+        container nginxCont
+        imagePullSecret "test"
+    }
+
+    let nginxPod = pod {
+        metadata meta
+        spec nginxPodSpec
+    }
+
+    nginxPod.ApiVersion |> should equal "v1"
+    nginxPod.Kind |> should equal "Pod"
+    nginxPod.Metadata.Name |> should equal meta.Name
+    nginxPod.Metadata.NamespaceProperty |> should equal meta.NamespaceProperty
+    nginxPod.Spec.Containers |> should haveCount 1
+    nginxPod.Spec.ImagePullSecrets |> should haveCount 1
+    nginxPod.Spec.ImagePullSecrets[0].Name |> should equal "test" 
+    let cont = nginxPod.Spec.Containers.[0]
+    cont.Name |> should equal "nginx"
+    cont.Image |> should equal "nginx:latest"
+    cont.ImagePullPolicy |> should equal "IfNotPresent"
+    cont.Env |> should haveCount 1
+    let env = cont.Env.[0]
+    env.Name |> should equal "PORT"
+    env.Value |> should equal "8080"
+    cont.Ports |> should haveCount 1
+    let port = cont.Ports.[0]
+    port.Protocol |> should equal "TCP"
+    port.ContainerPort |> should equal 8080
+    cont.Command |> Seq.toList |> should matchList (["nginx"; "-g"; "daemon off;"])
+    
+
+
+
+[<Fact>]
+let ``Test pod 2`` () =
+    let meta = metadata {
+        name "test"
+        nmspc "test"
+        labels [Label("app", "test"); Label("server", "nginx")]
+    }
+
+    let nginxCont = container {
+        name "nginx"
+        image (Image("nginx", Latest))
+        image_pull_policy (IfNotPresent)
+        command ["nginx"; "-g"; "daemon off;"]
+        env [NameValue("PORT", "8080")]
+        ports [TCP(8080)]
+    }   
+
+    let nginxPod = pod {
+        metadata meta
+        container nginxCont
+    }
+
+    nginxPod.ApiVersion |> should equal "v1"
+    nginxPod.Kind |> should equal "Pod"
+    nginxPod.Metadata.Name |> should equal meta.Name
+    nginxPod.Metadata.NamespaceProperty |> should equal meta.NamespaceProperty
+    nginxPod.Spec.Containers |> should haveCount 1
+    let cont = nginxPod.Spec.Containers.[0]
+    cont.Name |> should equal "nginx"
+    cont.Image |> should equal "nginx:latest"
+    cont.ImagePullPolicy |> should equal "IfNotPresent"
+    cont.Env |> should haveCount 1
+    let env = cont.Env.[0]
+    env.Name |> should equal "PORT"
+    env.Value |> should equal "8080"
+    cont.Ports |> should haveCount 1
+    let port = cont.Ports.[0]
+    port.Protocol |> should equal "TCP"
+    port.ContainerPort |> should equal 8080
+    cont.Command |> Seq.toList |> should matchList (["nginx"; "-g"; "daemon off;"])
